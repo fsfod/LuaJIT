75024b90520780c48379a53a99373db993e0d464
fsfod <fsfod11@gmail.com>
2015-06-20 08:45:05 +0100

    Implement a basic GC stats lib that walks the GC linked list and counts the number and size of Lua object types like tables and strings

---
+++ src/lj_gcstats.h
@@ -0,0 +1,54 @@
+/*
+** GC snapshot and stats system
+*/
+#ifndef gcstats_h
+#define gcstats_h
+
+#include "lua.h"
+#include <stdint.h>
+
+typedef struct GCObjStat {
+  size_t count;
+  size_t totalsize;
+  size_t maxsize;
+} GCObjStat;
+
+typedef enum GCObjType {
+  GCObjType_String,
+  GCObjType_Upvalue,
+  GCObjType_Thread,
+  GCObjType_FuncPrototype,
+  GCObjType_Function,
+  GCObjType_Trace,
+  GCObjType_CData,
+  GCObjType_Table,
+  GCObjType_UData,
+  GCObjType_Max,
+} GCObjType;
+
+typedef struct GCStatsTable {
+  uint32_t arraysize;
+  uint32_t arraycapacity;
+  uint32_t hashsize;
+  uint32_t hashcapacity;
+  uint32_t hashcollisions;
+} GCStatsTable;
+
+typedef struct GCStats {
+  GCObjStat objstats[GCObjType_Max];
+  
+  GCStatsTable registry;
+  GCStatsTable globals;
+  int finlizercdata_count;
+} GCStats;
+
+LUA_API void gcstats_collect(lua_State *L, GCStats* result);
+
+
+typedef struct SnapshotObj {
+  uint32_t typeandsize;
+  uint32_t address;
+} SnapshotObj;
+
+
+#endif
