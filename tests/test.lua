68515a2ea3cc4a2b0570a02679413a47d7512fa5
fsfod <fsfod11@gmail.com>
2015-10-16 06:45:55 +0100

    some more tests

--- tests/test.lua
+++ tests/test.lua
@@ -3,13 +3,25 @@ local jit_tester = require("jit_tester")
 local tracker = require("tracetracker")
 
 local testjit = jit_tester.testsingle
+local testexit = jit_tester.testexit
+local testnoexit = jit_tester.testnoexit
+
+local array = {}
+array[1] = "a"
+array[2] = "b"
+array[3] = "c"
+array[4] = "d"
+array[5] = "e"
+
+table.setreadonly(array)
 
 local htbl = {
   a = true, 
   b = false,
   c = 1,
-  d = "astring",
-  e = {},
+  d = 2,
+  e = "astring",
+  f = {},
 }
 
 htbl.self = htbl
@@ -18,6 +30,8 @@ table.setreadonly(htbl)
 
 local tab_mt = setmetatable({}, table.setreadonly({__index = htbl}))
 
+local tab_mt2 = setmetatable({}, {__index = array})
+
 local dump = require("jit.dump")
 --dump.on("tbirsmxa")
 --tracker.start()
@@ -25,27 +39,40 @@ local dump = require("jit.dump")
 
 table.setreadonly(_G)
 
+print("before loop")
+
 local function getvalue(k)
   local value = htbl[k]
   return value
 end
 
-
-
-print("before loop")
-
 testjit(true, getvalue, "a")
 testjit(false, getvalue, "b")
-testjit(1, getvalue, "c")
 
-local function getvaluemt(k)
-  local value = tab_mt[k]
+testjit(1, getvalue, "c")
+testnoexit(2, getvalue, "d")
+testexit(false, getvalue, "b")
+
+local function getvaluemt(tab,  k)
+  local value = tab[k]
   return value
 end
 
-testjit(true, getvaluemt, "a")
-testjit(false, getvaluemt, "b")
-testjit(1, getvaluemt, "c")
+testjit(true, getvaluemt, tab_mt, "a")
+testjit(false, getvaluemt, tab_mt, "b")
+testjit(1, getvaluemt, tab_mt, "c")
+
+testexit(nil, getvaluemt, tab_mt2, "a")
+
+
+local function getarrvalue(i)
+  local value = array[i]
+  return value
+end
+
+testjit("a", getarrvalue, 1)
+testjit("b", getarrvalue, 2)
+testjit("c", getarrvalue, 3)
 
 print("testspass")
 
