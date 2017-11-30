1cce2b4b11dd9085ea182581bbf314b00b974483
fsfod <fsfod11@gmail.com>
2017-11-30 15:41:33 +0000

    Add recording when a function proto is created/loaded

--- src/lj_jitlog.c
+++ src/lj_jitlog.c
@@ -230,6 +230,15 @@ static void jitlog_gcstate(jitlog_State *context, int newstate)
   log_gcstate(&context->ub, newstate, g->gc.state, g->gc.total, g->strnum);
 }
 
+static void jitlog_protoloaded(jitlog_State *context, GCproto *pt)
+{
+  if (jitlog_isfiltered(context, LOGFILTER_PROTO_LOADED)) {
+    return;
+  }
+  memorize_proto(context, pt);
+  log_protoloaded(&context->ub, pt);
+}
+
 static void free_context(jitlog_State *context);
 
 static void jitlog_loadstage2(lua_State *L, jitlog_State *context);
@@ -252,6 +261,9 @@ static void jitlog_callback(void *contextptr, lua_State *L, int eventid, void *e
       jitlog_traceflush(context, (FlushReason)(uintptr_t)eventdata);
       break;
 #endif
+    case VMEVENT_BC:
+      jitlog_protoloaded(context, (GCproto*)eventdata);
+      break;
     case VMEVENT_GC_STATECHANGE:
       jitlog_gcstate(context, (int)(uintptr_t)eventdata);
       break;
