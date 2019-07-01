ea395658bb627bcade915c06f4e3252bbe994315
fsfod <fsfod11@gmail.com>
2019-07-01 09:54:56 +0100

    Fix premake workspace files to generate relative paths to the solution

--- premake5.lua
+++ premake5.lua
@@ -46,8 +46,9 @@ premake.override(premake.vstudio.sln2005, "projects", function(base, wks)
   if wks.workspace_files and #wks.workspace_files > 0 then
     premake.push('Project("{2150E333-8FDC-42A3-9474-1A3956D46DE8}") = "Solution Items", "Solution Items", "{' .. os.uuid("Solution Items:"..wks.name) .. '}"')
     premake.push("ProjectSection(SolutionItems) = preProject")
-    for _, path in ipairs(wks.workspace_files) do
-      premake.w(path.." = "..path)
+    for _, file in ipairs(wks.workspace_files) do
+      file = path.rebase(file, ".", wks.location)
+      premake.w(file.." = "..file)
     end
     premake.pop("EndProjectSection")
     premake.pop("EndProject")
