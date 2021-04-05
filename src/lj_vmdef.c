ca661c134068fa1dc4c7c9bd3e2ccffd4f00cf8f
fsfod <fsfod11@gmail.com>
2021-04-05 17:47:38 +0100

    Add c version of vmdef.lua

---
+++ src/lj_vmdef.c
@@ -0,0 +1,231 @@
+#define LUA_CORE
+
+#include <math.h>
+#include <stdio.h>
+
+#include "lj_jit.h"
+#include "lj_vm.h"
+#include "lj_lib.h"
+#include "lj_trace.h"
+#include "lj_tab.h"
+#include "lj_gc.h"
+#include "lj_buf.h"
+#include "lj_ircall.h"
+#include "lj_target.h"
+#include "lj_frame.h"
+#include "lj_ctype.h"
+#include "lj_err.h"
+#include "lj_strscan.h"
+#include "lj_strfmt.h"
+#include "lj_prng.h"
+#include "lj_carith.h"
+#include "lj_cdata.h"
+#include "lj_serialize.h"
+#include "lj_vmdef.h"
+
+#define array_size(a) ((unsigned int)(sizeof(a) / sizeof((a)[0])))
+
+#define TYPELIST(_) \
+  _(TValue,   sizeof(TValue)) \
+  _(string,   sizeof(GCstr)) \
+  _(upvalue,  sizeof(GCupval)) \
+  _(thread,   sizeof(lua_State)) \
+  _(proto,    sizeof(GCproto)) \
+  _(function, sizeof(GCfunc)) \
+  _(trace,    sizeof(GCtrace)) \
+  _(cdata,    sizeof(GCcdata)) \
+  _(table,    sizeof(GCtab)) \
+  _(userdata, sizeof(GCudata)) \
+  _(GCfuncC,  sizeof(GCfuncC)) \
+  _(GCfuncL,  sizeof(GCfuncL)) \
+  _(GChead,   offsetof(GChead, unused1)) \
+  _(table_node,   sizeof(Node)) \
+  _(GG_State,     sizeof(GG_State)) \
+  _(global_State, sizeof(global_State)) \
+  _(GCState,      sizeof(GCState)) \
+  _(CTState,      sizeof(CTState)) \
+  _(jit_State,    sizeof(jit_State)) \
+
+#define SIZENUM(name, sz) sz,
+#define SIZENAME(name, sz) #name,
+
+const MSize reflect_typesizes[] = {
+  TYPELIST(SIZENUM)
+};
+
+const char *reflect_typenames[] = {
+  TYPELIST(SIZENAME)
+};
+
+#define REFLECT_FLDEF(_) \
+  _(str_len,	offsetof(GCstr, len)) \
+  _(str_hash,	offsetof(GCstr, hash)) \
+  _(func_env,	offsetof(GCfunc, l.env)) \
+  _(func_pc,	offsetof(GCfunc, l.pc)) \
+  _(func_ffid,	offsetof(GCfunc, l.ffid)) \
+  _(thread_env,	offsetof(lua_State, env)) \
+  _(tab_colo,	offsetof(GCtab, colo)) \
+  _(tab_meta,	offsetof(GCtab, metatable)) \
+  _(tab_array,	offsetof(GCtab, array)) \
+  _(tab_node,	offsetof(GCtab, node)) \
+  _(tab_asize,	offsetof(GCtab, asize)) \
+  _(tab_hmask,	offsetof(GCtab, hmask)) \
+  _(node_key,	offsetof(Node, key)) \
+  _(node_val,	offsetof(Node, val)) \
+  _(node_next,	offsetof(Node, next)) \
+  _(udata_meta,	offsetof(GCudata, metatable)) \
+  _(udata_env,	offsetof(GCudata, env)) \
+  _(udata_udtype, offsetof(GCudata, udtype)) \
+  _(cdata_ctypeid, offsetof(GCcdata, ctypeid)) \
+  _(gchead_gct, offsetof(GChead, gct)) \
+  _(gchead_marked, offsetof(GChead, marked))
+
+
+#define FLDOFS(name, sz) sz,
+#define FLDNAME(name, sz) #name,
+
+static const MSize reflect_offsets[] = {
+  REFLECT_FLDEF(FLDOFS)
+};
+
+static const char *const reflect_fieldnames[] = {
+  REFLECT_FLDEF(FLDNAME)
+};
+
+const VMReflect lj_vmreflect = {
+  .typenames = reflect_typenames,
+  .typesizes = reflect_typesizes,
+  .typecount = array_size(reflect_typenames),
+  .fieldnames = reflect_fieldnames,
+  .fieldoffsets = reflect_offsets,
+  .fieldcount = array_size(reflect_fieldnames),
+};
+
+static const char *const bc[] = {
+  #define BCNAME(name, ma, mb, mc, mt)       #name,
+  BCDEF(BCNAME)
+  #undef BCNAME
+};
+
+static const char *const fastfuncs[] = {
+  "Lua",
+  "C",
+  #define FFDEF(name)   #name,
+  #include "lj_ffdef.h"
+  #undef FFDEF
+};
+
+static const char *const gcstates[] = {
+  "pause",
+  "propagate",
+  "atomic",
+  "sweepstring",
+  "sweep",
+  "finalize",
+};
+
+LJ_STATIC_ASSERT(GCSpropagate == 1);
+LJ_STATIC_ASSERT(GCSatomic == 2);
+LJ_STATIC_ASSERT(GCSsweepstring == 3);
+LJ_STATIC_ASSERT(GCSsweep == 4);
+LJ_STATIC_ASSERT(GCSfinalize == 5);
+
+static const char *const gcatomic_stages[] = {
+  "stage_end",
+  "mark_upvalues",
+  "mark_roots",
+  "mark_grayagain",
+  "separate_udata",
+  "mark_udata",
+  "clearweak",
+};
+
+static const char *const flushreason[] = {
+  "other",
+  "user_requested",
+  "maxmcode",
+  "maxtrace",
+  "profile_toggle",
+  "set_builtinmt",
+  "set_immutableuv",
+  "jitlog_tracemarkers",
+};
+
+static const char * jitparams[] = {
+  #define PARAMNAME(len, name, value)	#name,
+  JIT_PARAMDEF(PARAMNAME)
+  #undef PARAMNAME
+};
+
+static const int32_t jit_param_default[JIT_P__MAX + 1] = {
+#define JIT_PARAMINIT(len, name, value)	(value),
+JIT_PARAMDEF(JIT_PARAMINIT)
+#undef JIT_PARAMINIT
+  0
+};
+
+static const char *const terror[] = {
+  #define TREDEF(name, msg)	#name,
+  #include "lj_traceerr.h"
+  #undef TREDEF
+};
+
+static const char *const trace_errors[] = {
+  #define TREDEF(name, msg)	msg,
+  #include "lj_traceerr.h"
+  #undef TREDEF
+};
+
+static const char *const ir[] = {
+  #define IRNAME(name, m, m1, m2)	#name,
+  IRDEF(IRNAME)
+  #undef IRNAME
+};
+
+static const char *const ir_types[] = {
+  #define IRTNAME(name, size)	#name,
+  IRTDEF(IRTNAME)
+  #undef IRTNAME
+};
+
+static const char *const ir_call[] = {
+  #define IRCALLNAME(cond, name, nargs, kind, type, flags)	#name,
+  IRCALLDEF(IRCALLNAME)
+  #undef IRCALLNAME
+};
+
+static const char* const ir_fpmath[] = {
+  #define IRFPMDEFNAME(name)	#name,
+  IRFPMDEF(IRFPMDEFNAME)
+  #undef IRFPMDEFNAME
+};
+
+static const char * ir_fields[] = {
+  #define FLNAME(name, ofs)	#name,
+  IRFLDEF(FLNAME)
+  #undef FLNAME
+};
+
+static const char *const trace_link[] = {
+  "none", "root", "loop", "tail-recursion", "up-recursion", "down-recursion",
+  "interpreter", "return", "stitch"
+};
+
+LJ_STATIC_ASSERT(array_size(trace_link)-1 == LJ_TRLINK_STITCH);
+
+static void* ircall_addr[] = {
+  #define IRCALLNAME(cond, name, nargs, kind, type, flags) (ASMFunction)IRCALLCOND_##cond(name),
+  IRCALLDEF(IRCALLNAME)
+  #undef IRCALLNAME
+};
+
+
+#define decl_enumdef(name) \
+  .name = {.names = name, .count = sizeof(name)/sizeof((name)[0])},
+
+const VMdef lj_vmdef = {
+  VMENUMDEFS(decl_enumdef)
+ .irmode = lj_ir_mode,
+ .jitparam_defaults = jit_param_default,
+ .ir_calladdr = ircall_addr,
+};
---
+++ src/lj_vmdef.h
@@ -0,0 +1,60 @@
+#ifndef _LJ_REFLECT_H
+#define _LJ_REFLECT_H
+
+#include <stdint.h>
+
+#define VMENUMDEFS(_) \
+  _(bc) \
+  _(fastfuncs) \
+  _(gcstates) \
+  _(gcatomic_stages) \
+  _(ir) \
+  _(ir_types) \
+  _(ir_fields) \
+  _(ir_call) \
+  _(ir_fpmath) \
+  _(terror) \
+  _(trace_errors) \
+  _(trace_link) \
+  _(jitparams) \
+  _(flushreason) \
+
+typedef struct VMEnumDef {
+  const char* const* names;
+  size_t count;
+} VMEnumDef;
+
+typedef struct VMdef {
+  VMEnumDef bc;
+  VMEnumDef fastfuncs;
+  VMEnumDef gcstates;
+  VMEnumDef gcatomic_stages;
+  VMEnumDef ir;
+  VMEnumDef ir_types;
+  VMEnumDef ir_fields;
+  VMEnumDef ir_call;
+  VMEnumDef ir_fpmath;
+  VMEnumDef trlink;
+  VMEnumDef terror;
+  VMEnumDef trace_errors;
+  VMEnumDef trace_link;
+  VMEnumDef flushreason;
+  VMEnumDef jitparams;
+  int* jitparam_defaults;
+  uint8_t *irmode;
+  void** ir_calladdr;
+} VMdef;
+
+typedef struct VMReflect {
+  int typecount;
+  uint32_t* typesizes;
+  const char* const* typenames;
+  int fieldcount;
+  const char* const* fieldnames;
+  uint32_t* fieldoffsets;
+} VMReflect;
+
+extern const VMdef lj_vmdef;
+extern const VMReflect lj_vmreflect;
+
+#endif
