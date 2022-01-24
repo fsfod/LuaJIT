b486f8dbb3b916577dfca004e356567b44f81b6b
fsfod <fsfod11@gmail.com>
2022-01-24 04:20:21 +0000

    Add jitlog_getjlctx to fetch the currently active JITLog for a Lua state and expose jitlog_isrunning

--- src/jitlog.h
+++ src/jitlog.h
@@ -97,6 +97,8 @@ LUA_API int jitlog_isrunning(lua_State *L);
 LUA_API JITLogUserContext* jitlog_getjlctx(lua_State *L);
 /* Start the JITLog while the Lua state is still running code in another OS thread */
 LUA_API JITLogUserContext* jitlog_startasync(lua_State* L, UserBuf *sink);
+LUA_API int jitlog_isrunning(lua_State *L); 
+LUA_API JITLogUserContext* jitlog_getjlctx(lua_State *L);
 LUA_API void jitlog_close(JITLogUserContext *usrcontext);
 LUA_API int jitlog_save(JITLogUserContext *usrcontext, const char *path);
 LUA_API int jitlog_flush(JITLogUserContext* usrcontext);
