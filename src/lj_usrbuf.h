7b56ab4e784afb9cbe2193aacb980eee7e725184
fsfod <fsfod11@gmail.com>
2022-09-27 12:34:49 +0100

    Add optional fields for custom UserBuf flush function and reserved header space size to UBufInitArgs

--- src/lj_usrbuf.h
+++ src/lj_usrbuf.h
@@ -28,6 +28,7 @@ struct UserBuf;
 typedef struct UserBuf UserBuf;
 
 typedef int (*UBufHandler)(UserBuf *buff, UBufAction action, void *arg);
+typedef size_t(*FlushBufferCallback)(void *ud, struct UserBuf *ub, char* data, size_t size);
 
 typedef struct UserBuf {
   /* These could point to a plain buffer or a mem mapped file */
@@ -43,6 +44,10 @@ typedef struct UserBuf {
 typedef struct UBufInitArgs {
   const char *path;
   size_t minbufspace;
+  FlushBufferCallback flushcb;
+  void* flush_ud;
+  int hdrspace;
+  uint32_t target_capacity; /* target size to flush the buffer at */
 } UBufInitArgs;
 
 #define ubufsz(ub) ((size_t)((ub)->e - (ub)->b))
